25d4fb639950c1a22578257fe56d1ca13010c23e
Wilhelm Mindhammar <wilhelm.karl.mindhammar@gmail.com>
2024-11-22 14:06:09 +0100

    Update PlayerController.cs

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public UnityEvent onAction1;
+   public UnityEvent onAction2;
+   public UnityEvent onAction3;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+   public void Action1 ()
+   {
+       onAction1.Invoke();
+   }
+   
+   public void Action2 ()
+   {
+       onAction2.Invoke();
+   }
+   
+   public void Action3 ()
+   {
+       onAction3.Invoke();
+   }
+
+
 }
