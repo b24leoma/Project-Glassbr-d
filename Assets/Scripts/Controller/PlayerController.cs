beab966b4248f81f3b7de89ec20a4e0eac30fe33
Wilhelm Mindhammar <wilhelm.karl.mindhammar@gmail.com>
2024-11-22 14:22:09 +0100

    Input action + start på playercontoller

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -2,13 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 
 public class PlayerController : MonoBehaviour
-{
+{ 
+    public InputActionReference pointerPositionReference;
    public UnityEvent onAction1;
    public UnityEvent onAction2;
    public UnityEvent onAction3;
+
+   private Vector2 mousePostion;
+
+   public void MousePosition()
+   {
+       // se https://docs.unity3d.com/ScriptReference/Camera.ScreenToWorldPoint.html
+   }
+   
+       
+   
 
 
    public void Action1 ()
