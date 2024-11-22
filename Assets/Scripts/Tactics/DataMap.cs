eb1340b18898175a53f9e901dcee511df1d41ffd
Mindhammar <wilhelm.karl.mindhammar@gmail.com>
2024-11-22 11:19:39 +0100

    Added prototype modular highlight system
    To Do : Integrate with MapManager, Add AOE logic,  Integration with future pathfinding

--- Assets/Scripts/Tactics/DataMap.cs
+++ Assets/Scripts/Tactics/DataMap.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.Tilemaps;
 
 public class DataMap : MonoBehaviour
@@ -7,9 +9,26 @@ public class DataMap : MonoBehaviour
     [SerializeField] private Tilemap dataMap;
 
     [SerializeField] private float maxDataValue;
+    
+    [SerializeField] Color maxDataColor, minDataColor, clearDataColor;
+
+    [SerializeField] private TurnBasedController turnBasedController;
+
+    [SerializeField] private float AddDataValue;
 
     private Dictionary<Vector3Int, float> dataTiles = new Dictionary<Vector3Int, float>();
 
+
+
+    public void AddData(Vector2 worldPosition, float dataAmount)
+    {
+        Vector3Int gridPosition = dataMap.WorldToCell(worldPosition);
+        ChangeDataValue(gridPosition, dataAmount);
+    }
+    
+    
+    
+    
     private void ChangeDataValue(Vector3Int gridPosition, float changeBy)
     {
         if  (!dataTiles.ContainsKey(gridPosition))
@@ -19,6 +38,10 @@ public class DataMap : MonoBehaviour
         if (newValue <= 0)
         {
             dataTiles.Remove(gridPosition);
+            
+            dataMap.SetTileFlags(gridPosition, TileFlags.None);
+            dataMap.SetColor(gridPosition, clearDataColor);
+            dataMap.SetTileFlags(gridPosition, TileFlags.LockColor);
         }
         else
         {
@@ -26,4 +49,29 @@ public class DataMap : MonoBehaviour
         }
     }
 
+    private void VisualizeData()
+    {
+        foreach (var entry in dataTiles)
+        {
+            float dataPercent = entry.Value / maxDataValue;
+            Color newTileColor = maxDataColor * dataPercent + minDataColor * (1f - dataPercent);
+
+            dataMap.SetTileFlags(entry.Key, TileFlags.None);
+            dataMap.SetColor(entry.Key, newTileColor);
+            dataMap.SetTileFlags(entry.Key, TileFlags.LockColor);
+        }
+    }
+
+    public void ClickedTile ()
+    {
+        Vector2 mouseCameraPosition = turnBasedController.PointerPosition;
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(mouseCameraPosition);
+        
+        AddData(mousePosition, AddDataValue);
+        
+        
+        VisualizeData();
+
+
+    }
 }
