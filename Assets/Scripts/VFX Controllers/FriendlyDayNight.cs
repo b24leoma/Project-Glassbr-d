e98fe9face6ae5d43b8d9d4115ea06b8fec36110
Wilhelm Mindhammar <wilhelm.karl.mindhammar@gmail.com>
2024-12-17 13:55:30 +0100

    DayNight

--- Assets/Scripts/VFX Controllers/FriendlyDayNight.cs
+++ Assets/Scripts/VFX Controllers/FriendlyDayNight.cs
@@ -1,67 +1,61 @@
-using System;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using DG.Tweening;
-using Color = System.Drawing.Color;
+using Random = System.Random;
 
 public class FriendlyDayNight : MonoBehaviour
 {
+    [SerializeField] private float duration;
+    [SerializeField] private Gradient gradient;
 
+    private Light2D targetLight;
+     [SerializeField] private float gradientPercent;
+     [Range(0f,1f),SerializeField] private float endValue = 1;
 
-  [SerializeField] private float duration;
-  [SerializeField]   private Gradient gradient;
-  
-  private Light2D _targetLight;
-  private float _currentTime;
-  private float _startTime;
-  [SerializeField] private float _gradientPercent;
-  [SerializeField]   private float _gradientEnd = 1f;
-  [SerializeField] private float _gradientStart;
-  [SerializeField] private float _endValue;
-  [SerializeField] private bool _toEnd;
-  
+    [SerializeField] private Ease ease;
+    [SerializeField] private LoopType loopType;
 
- [SerializeField] private float uno;
-  [SerializeField] private float dos;
-  
+    [SerializeField] private int cycleCount;
+    [SerializeField] private int dygnCount;
+    [SerializeField] private ParticleSystem fog;
+    [Range(0, 10), SerializeField] private float chanceForNoFog = 8f;
 
-  [SerializeField]  private Ease ease;
-  [SerializeField]      private LoopType loopType;
+    [SerializeField] private int slumptal;
 
-  [SerializeField] private int dygncount;
+    private void Awake()
+    {
+        targetLight = GetComponent<Light2D>();
+        gradientPercent = 0f;
+        StartDygn();
+    }
 
+    private void StartDygn()
+    {
+        DOTween.To(() => gradientPercent, x => gradientPercent = x, endValue, duration)
+            .SetLoops(-1, loopType)
+            .SetEase(ease)
+            .OnStepComplete(() => CycleCounter());
+    }
 
-  private void Awake()
-  {
-    _targetLight = GetComponent<Light2D>();
-    _startTime = Time.time;
-    _gradientPercent = 0f;
+    private void Update()
+    {
+        targetLight.color = gradient.Evaluate(gradientPercent);
+    }
 
-   
+    private void CycleCounter()
+    {
+        cycleCount++;
+        dygnCount = cycleCount / 2;
+        if (cycleCount % 2 != 0)
+        {
+            var randomNumber = new Random();
+            slumptal = randomNumber.Next(10);
 
-    StartDygn();
-  }
-
-
-  private void StartDygn()
-  {
-
-    _endValue = _toEnd ? _gradientEnd : _gradientStart;
-
-    DOTween.To(() => _gradientPercent, x => _gradientPercent = x, 1, duration).SetLoops(-1, loopType).SetEase(ease);
-
-
-    _toEnd = !_toEnd;
-
-
-  }
-
-
-  private void Update()
-  {
-    _targetLight.color = gradient.Evaluate(_gradientPercent);
-      
-      
-    
-  }
+            if (slumptal >= chanceForNoFog)
+            {
+                Debug.Log("Playing fog, should last for 60 seconds...");
+                fog.Play();
+            }
+        }
+    }
 }
