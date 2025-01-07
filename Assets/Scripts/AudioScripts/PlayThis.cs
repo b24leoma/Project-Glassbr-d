c6ebba39c30e13662765efa42b96f6b36feea9c4
Mindhammar <wilhelm.karl.mindhammar@gmail.com>
2025-01-07 20:40:13 +0100

    PAGEFLIP SOUNDS

--- Assets/Scripts/AudioScripts/PlayThis.cs
+++ Assets/Scripts/AudioScripts/PlayThis.cs
@@ -7,16 +7,20 @@ public class PlayThis : MonoBehaviour
     public void PlayDefeatSound()
     {
         FMODManager.instance.OneShot("Lost");
-
     }
 
     public void PlayUISelect()
-        {
-            FMODManager.instance.OneShot("UISelect");
-        }
-    
-        public void PlayUIClick()
-        {
-            FMODManager.instance.OneShot("UIClick");
-        }
+    {
+        FMODManager.instance.OneShot("UISelect");
+    }
+
+    public void PlayUIClick()
+    {
+        FMODManager.instance.OneShot("UIClick");
+    }
+
+    public void PlayPageFlip()
+    {
+        FMODManager.instance.OneShot("PageFlip");
+    }
     }
